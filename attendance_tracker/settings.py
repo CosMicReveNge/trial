3927add2f7189e6a91f69d41f74876ec89463a58
Vedant Agarwal <vedantagarwal13@gmail.com>
2025-07-03 01:28:02 +0530

    Prepare for Render deployment2

--- attendance_tracker/settings.py
+++ attendance_tracker/settings.py
@@ -88,7 +88,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [BASE_DIR / 'static']
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # ← This should NOT be the same as STATIC_ROOT
+]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
